3093a1414eba8fc98b6fc566d5695156a0c5177a
Nils Leusmann <leusmann@uni-bremen.de>
2020-08-17 09:28:01 +0200

    Adds the new Base-Classe. If there is an implementation in SDFParser, it is impossible to start the project

--- Source/URoboSimEditor/Public/SDFParser.h
+++ Source/URoboSimEditor/Public/SDFParser.h
@@ -9,6 +9,7 @@
 #include "SDF/SDFGeometry.h"
 #include "AssetRegistryModule.h"
 #include "ObjectMacros.h" // EObjectFlags
+#include "SDFParserBase.h"
 
 // Forward declaration
 class FXmlFile;
@@ -22,21 +23,11 @@ class USDFVisual;
 class USDFCollision;
 // class USDFGeometry;
 
-/**
-*
-*/
-enum class ESDFType : uint8
-{
-	None,
-	Visual,
-	Collision
-};
-
 
 /**
 * SDF parser class
 */
-class UROBOSIM_API FSDFParser
+class UROBOSIM_API FSDFParser : FSDFParserBase
 {
 public:
 	// Default constructor
@@ -48,63 +39,23 @@ public:
 	// Destructor
 	~FSDFParser();
 
-	// Load sdf from path
-	bool LoadSDF(const FString& InFilename);
-
-	// Clear parser
-	void Clear();
+    virtual bool LoadSDF (const FString& InFilename) override;
 
 	// Create data asset and parse sdf data into it
-	USDFDataAsset* ParseToNewDataAsset(UObject* InParent, FName InName, EObjectFlags InFlags);
+    virtual USDFDataAsset* ParseToNewDataAsset(UObject* InParent, FName InName, EObjectFlags InFlags) override;
 
-private:
-	/* Begin parser functions */
-	// Check if sdf data is valid
-	bool IsValidSDF();
+protected:
+    /* Begin parser functions */
 
-	// Parse <sdf> node
-	void ParseSDF();
-
-	// Parse <model> node
-	void ParseModel(const FXmlNode* InNode);
 
 	// Parse <link> node
-	void ParseLink(const FXmlNode* InNode, USDFModel*& OutModel);
-
-	// Parse <link> <inertial> node
-	void ParseLinkInertial(const FXmlNode* InNode, USDFLink*& OutLink);
-
-	// Parse <visual> node
-	void ParseVisual(const FXmlNode* InNode, USDFLink*& OutLink);
-
-	// Parse <collision> node
-	void ParseCollision(const FXmlNode* InNode, USDFLink*& OutLink);
-
-	// Parse <geometry> node
-	void ParseGeometry(const FXmlNode* InNode, USDFGeometry*& OutGeometry, ESDFType Type);
+    virtual void ParseVisual(const FXmlNode* InNode, USDFLink*& OutLink) override; //possible there are some errors for the runtime verison
 
 	// Parse <geometry> <mesh> node
-	void ParseGeometryMesh(const FXmlNode* InNode, USDFGeometry*& OutGeometry, ESDFType Type);
+    virtual void ParseGeometryMesh(const FXmlNode* InNode, USDFGeometry*& OutGeometry, ESDFType Type) override; // Also maybe
 
-	// Parse <geometry> <box> node
-	void ParseGeometryBox(const FXmlNode* InNode, USDFGeometry*& OutGeometry);
-
-	// Parse <geometry> <cylinder> node
-	void ParseGeometryCylinder(const FXmlNode* InNode, USDFGeometry*& OutGeometry);
-
-	// Parse <geometry> <sphere> node
-	void ParseGeometrySphere(const FXmlNode* InNode, USDFGeometry*& OutGeometry);
-
-	// Parse <joint> node
-	void ParseJoint(const FXmlNode* InNode, USDFModel*& OutModel);
-
-	// Parse <joint> <axis> node
-	void ParseJointAxis(const FXmlNode* InNode, USDFJoint*& OutJoint);
-
-	// Parse <joint> <axis> <limit> node
-	void ParseJointAxisLimit(const FXmlNode* InNode, USDFJoint*& OutJoint);
 	/* End parser functions */
-
+    virtual void ParseCollision(const FXmlNode* InNode, USDFLink*& OutLink) override;
 
 	/* Begin helper functions */
 	// Fix file path
@@ -122,30 +73,10 @@ private:
 	UStaticMesh* ImportMesh(const FString& Uri, ESDFType Type);
 	UStaticMesh* CreateMesh(ESDFType InType, ESDFGeometryType InShape, FString InName, TArray<float> InParameters);
 
-	// From <pose>z y z r p y</pose> to FTransform
-	FTransform PoseContentToFTransform(const FString& InPoseData);
-
-	// From <size>z y z</size> to FVector
-	FVector SizeToFVector(const FString& InSizeData);
-
-	// From <xzy>z y z</xzy> to FVector
-	FVector XyzToFVector(const FString& InXyzData);
 	/* End helper functions */
-
-
-	// Reader for the xml file
-	FXmlFile* XmlFile;
-
-	// Flag if parser is loaded
-	bool bSDFLoaded;
-
-	// Pointer to the generated data asset
-	USDFDataAsset* DataAsset;
 
 	// Cached directory path
 	FString DirPath;
-
-	FString CurrentLinkName;
 
 	// Fbx factory
 	UFbxFactory* FbxFactory;
