44479d3fb0e4b56e56c9acb716501df475b359ea
Giang Nguyen <hoanggia@uni-bremen.de>
2021-03-24 15:35:47 +0100

    Revert "Fix offset and axis"
    This reverts commit 8a012fe797676937e73c4acb51be97777aad1afe.

--- Source/URoboSim/Classes/Factory/RJointFactory.h
+++ Source/URoboSim/Classes/Factory/RJointFactory.h
@@ -24,6 +24,7 @@ protected:
     virtual void SetJointParameters();
     virtual void CreateConstraint();
     virtual void SetJointLimit();
+    virtual void RotateConstraintToRefAxis();
     virtual float CalculateRotationOffset();
 
   UPROPERTY()
