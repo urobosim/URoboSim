43351e94c26d7a85b6e4964e6f128282305a07c6
Michael Neumann <neumann.michael@uni-bremen.de>
2021-04-21 10:14:52 +0200

    change clone to submodule in install file

--- install.bash
+++ install.bash
@@ -7,25 +7,26 @@ cd ..
 
 if [ ! -d URoboVision/.git ]
 then
-    git clone git@github.com:K4R-IAI/K4R_Development.git
+    # git clone git@github.com:urobosim/URoboVision.git
+    git submodule add git@github.com:urobosim/URoboVision.git
 fi
 
 if [ ! -d UROSBridge/.git ]
 then
-    git clone git@github.com:urobosim/UROSBridge.git
+    git submodule add git@github.com:urobosim/UROSBridge.git
 fi
 
 if [ ! -d UROSWorldControl/.git ]
 then
-    git clone git@github.com:urobosim/UROSWorldControl.git
+    git submodule add git@github.com:urobosim/UROSWorldControl.git
 fi
 
 if [ ! -d UTFPublisher/.git ]
 then
-    git clone git@github.com:NeumannM89/UTFPublisher.git
+    git submodule add git@github.com:NeumannM89/UTFPublisher.git
 fi
 
 if [ ! -d UUtils/.git ]
 then
-    git clone git@github.com:urobosim/UUtils.git
+    git submodule add git@github.com:urobosim/UUtils.git
 fi
