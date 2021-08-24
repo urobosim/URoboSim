99a447ebbfaa0bfbd1b0873ca79a6e0b282b2b6c
Michael Neumann <neumann.michael@uni-bremen.de>
2021-08-24 15:57:08 +0200

    change category of SDFJointAxis variables

--- Source/URoboSim/Classes/SDF/SDFJointAxis.h
+++ Source/URoboSim/Classes/SDF/SDFJointAxis.h
@@ -19,22 +19,22 @@ public:
 
 	USDFJointAxis();
 
-	UPROPERTY(EditAnywhere, Category = "Joint|Axis")
+	UPROPERTY(EditAnywhere, Category = "Axis")
 	FVector Xyz;
 
-	UPROPERTY(EditAnywhere, Category = "Joint|Axis")
+	UPROPERTY(EditAnywhere, Category = "Axis")
 	bool bUseParentModelFrame;
 
-	UPROPERTY(EditAnywhere, Category = "Joint|Axis|Limit")
+	UPROPERTY(EditAnywhere, Category = "Axis|Limit")
 	float Lower;
 
-	UPROPERTY(EditAnywhere, Category = "Joint|Axis|Limit")
+	UPROPERTY(EditAnywhere, Category = "Axis|Limit")
 	float Upper;
 
-	UPROPERTY(EditAnywhere, Category = "Joint|Axis|Limit")
+	UPROPERTY(EditAnywhere, Category = "Axis|Limit")
 	float Effort;
 
-	UPROPERTY(EditAnywhere, Category = "Joint|Axis|Limit")
+	UPROPERTY(EditAnywhere, Category = "Axis|Limit")
 	float Velocity;
 
 	void SetLowerLimitFromSDF(float LowerLimit);
