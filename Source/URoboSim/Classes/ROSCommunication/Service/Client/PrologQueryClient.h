96f150dc9e30ed2c7844a80f726501a413f671a0
Michael Neumann <neumann.michael@uni-bremen.de>
2021-09-06 12:36:22 +0200

    add PrologQueryClient

---
+++ Source/URoboSim/Classes/ROSCommunication/Service/Client/PrologQueryClient.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "RServiceClient.h"
+#include "ROSBridgeSrvClient.h"
+#include "json_prolog_msgs/srv/PrologQuery.h"
+// clang-format off
+#include "PrologQueryClient.generated.h"
+// clang-format on
+
+UCLASS()
+class UROBOSIM_API UPrologQueryClientParameter final : public URServiceClientParameter
+{
+  GENERATED_BODY()
+
+public:
+  UPrologQueryClientParameter()
+  {
+    ServiceName = TEXT("rosprolog/query");
+    ServiceType = TEXT("json_prolog_msgs/PrologQuery");
+  }
+
+public:
+};
+
+UCLASS()
+class UROBOSIM_API UPrologQueryClient final : public UServiceClient
+{
+  GENERATED_BODY()
+
+public:
+  UPrologQueryClient();
+
+public:
+  void SetServiceClientParameters(URServiceClientParameter *&ServiceClientParameters) override;
+
+  void CallService() override;
+
+  FString Query;
+protected:
+  void CreateServiceClient() override;
+
+public:
+  void SendQuery(const uint Mode, const FString& Id, const FString& InQuery, FTimerManager& InTimerManager);
+
+public:
+
+private:
+  TSharedPtr<class FPrologQueryClient> ServiceClient;
+
+  TSharedPtr<json_prolog_msgs::PrologQuerySrv::Request> Request;
+
+  TSharedPtr<json_prolog_msgs::PrologQuerySrv::Response> Response;
+};
+
+class FPrologQueryClient final : public FROSBridgeSrvClient
+{
+public:
+  FPrologQueryClient(const FString &InName, const FString &InType, const FString &InQuery);
+
+  void Callback(TSharedPtr<FROSBridgeSrv::SrvResponse> InResponse) override;
+
+private:
+  uint Mode;
+  FString Query;
+  FString Id;
+};
